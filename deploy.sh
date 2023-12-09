21e8633053e47342982c39be2a1ba8c72c14df15
mgahed <abdelrhmanmgahed131@gmail.com>
2023-12-09 20:36:57 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,11 @@ php artisan config:cache
 echo "Running migrations..."
 php artisan migrate --force
 
+echo "Running npm install..."
+npm install
+
+echo "Running npm run dev..."
+npm run dev
+
 echo "Running server..."
 php artisan serve --host=0.0.0.0 --port=80
